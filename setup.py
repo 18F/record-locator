9e6c31f0c3181ceb411c2b7fadfa3a0e2815d3b5
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-05-01 18:34:12 +0000

    Add in distribution setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+readme = open('README.md').read()
+
+setup(
+    name="RecordLocator",
+    version="0.0",
+    packages=['recordlocator'],
+    description="Generate airline style record locators",
+    license="Public Domain",
+    long_description=readme,
+)
