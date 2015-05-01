1d71c93250efc13374b74af4c852843e2cec4468
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-05-01 19:38:26 +0000

    Add in the inital record locator generation

---
+++ recordlocator/generator.py
@@ -0,0 +1,19 @@
+try:
+    # Apparently not all implementations have SystemRandom defined, so I expect
+    # this import to fail, if that's the case.
+    from random import SystemRandom
+    randomizer = SystemRandom()
+except:
+    import random as randomizer
+
+# This alphabet removes vowels, make it very hard for this to
+# generate words. This is for profanity avoidance.
+# It also removes characters that in poor fonts might be
+# confusing such as B,8,5,S,0,O,1,I,Q
+SAFE_ALPHABET_STRING = '1234679CDFGHJKMNPQRTVWXYZ'
+SAFE_ALPHABET = list(SAFE_ALPHABET_STRING)
+
+
+def generate(length=8):
+    """This generates a random travel-style record locator."""
+    return ''.join(randomizer.choice(SAFE_ALPHABET) for i in range(0, length))
