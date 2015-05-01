a4509ff4d91d4138a832e5eac6536095ef965cb1
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-05-01 21:34:07 +0000

    Make the safe record locator generation more explicit

--- recordlocator/generator.py
+++ recordlocator/generator.py
@@ -10,10 +10,12 @@ except:
 # generate words. This is for profanity avoidance.
 # It also removes characters that in poor fonts might be
 # confusing such as B,8,5,S,0,O,1,I,Q
-SAFE_ALPHABET_STRING = '1234679CDFGHJKMNPQRTVWXYZ'
+SAFE_ALPHABET_STRING = '234679CDFGHJKMNPRTVWXYZ'
 SAFE_ALPHABET = list(SAFE_ALPHABET_STRING)
 
 
-def generate(length=8):
-    """This generates a random travel-style record locator."""
+def safe_generate(length=8):
+    """This generates a random travel-style record locator using the safe
+    alphabet. """
+
     return ''.join(randomizer.choice(SAFE_ALPHABET) for i in range(0, length))
--- tests/test_generation.py
+++ tests/test_generation.py
@@ -5,8 +5,11 @@ from recordlocator import generator
 class TestGenerator(unittest.TestCase):
 
     def test_default_generation(self):
-        """ The default locator length is 8. """
+        """ The default locator length is 8. Also check some basic things
+        about the locator. """
 
-        locator = generator.generate()
+        locator = generator.safe_generate()
         self.assertEqual(len(locator), 8)
         self.assertTrue(locator.isupper())
+        for c in ['B', '8', '5', 'S', '0', 'O', '1', 'I', 'Q']:
+            self.assertTrue(c not in locator)
