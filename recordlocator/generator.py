f25f1b775468dbd6ce7dd4a2abe6635728d25359
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2015-06-02 20:31:28 +0000

    Add a new timestamp-based record locator method

--- recordlocator/generator.py
+++ recordlocator/generator.py
@@ -6,6 +6,8 @@ try:
 except:
     import random as randomizer
 
+import datetime
+
 # This alphabet removes vowels (and the letter 'V' which looks like a 'U'),
 # make it very hard for this to generate words. This is for profanity
 # avoidance.  It also removes characters that in poor fonts might be confusing
@@ -20,3 +22,13 @@ def safe_generate(length=8):
     alphabet. """
 
     return ''.join(randomizer.choice(SAFE_ALPHABET) for i in range(0, length))
+
+def timestamp_generate():
+    """ This uses a combination of random digits and microseconds to generate a
+    unique value. This is intended to be used in instances where
+    safe_generate() keep producing duplicates. """
+
+    microsecond = datetime.datetime.now().microsecond
+    remaining_length = 16 - (len(str(microsecond)) + 1)
+    random = safe_generate(remaining_length)
+    return '%s-%s' % (random, microsecond)
